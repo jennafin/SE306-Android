bec7718dc3f5d23fca9fd89725ade02791bab2ca
Nicholas Weatherburn <n.weatherburn@gmail.com>
2014-09-28 22:48:47 +1300

    Added files I forgot before.

---
+++ LucidDreamer/Assets/scripts/GroundGeneratorScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGeneratorScript : MonoBehaviour {
+	
+	public GameObject groundObject;
+	float spawnRate = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	void Spawn () {
+		Instantiate(groundObject, transform.position, Quaternion.identity);
+		Invoke ("Spawn", spawnRate);
+	}
+}
