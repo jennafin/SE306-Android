69387eb7651bdbc8cfac16a1c1d643c32aedbdda
hbateman <hugobateman@gmail.com>
2014-10-05 18:34:11 +1300

    Created enemies branch, started work on stationary and moving enemies

---
+++ LucidDreamer/Assets/scripts/MovingEnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingEnemyScript : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (Vector2.one * moveSpeed);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			Debug.Log("Collision Detected on mover");
+		
+	}
+}
