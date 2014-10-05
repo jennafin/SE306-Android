996f1c74e708e91c32333d93ca18882370935bf9
hbateman <hugobateman@gmail.com>
2014-10-05 21:15:35 +1300

    Added Spawners for moving and stationary enemies

---
+++ LucidDreamer/Assets/scripts/EnemySpawnScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnScript : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float spawnMax = 1f;
+	public float spawnMin = 2f;
+
+	void Start () {
+
+		Spawn();
+	
+	}
+	
+	void Spawn() {
+
+		Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+		Invoke("Spawn", Random.Range (spawnMin, spawnMax));
+
+	}
+}
