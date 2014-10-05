b221515831014c3968c1fecad00e56e7cc89ec2d
hbateman <hugobateman@gmail.com>
2014-10-05 20:40:30 +1300

    Update to moving enemy

--- LucidDreamer/Assets/scripts/MovingEnemyScript.cs
+++ LucidDreamer/Assets/scripts/MovingEnemyScript.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class MovingEnemyScript : MonoBehaviour {
 
-	public float moveSpeed;
+	public float walkSpeed = 5.0f;
+	public float wallLeft = 0.0f;
+	public float wallRight = 2.0f;
 
-	// Use this for initialization
+	float walkingDirection = 1.0f;
+	Vector3 walkAmount;
+
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		transform.Translate (Vector2.one * moveSpeed);
 
+		walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+		
+		if (walkingDirection > 0.0f && transform.position.x >= wallRight)
+			walkingDirection = -1.0f;
+		else if (walkingDirection < 0.0f && transform.position.x <= wallLeft)
+			walkingDirection = 1.0f;
+		
+		transform.Translate(walkAmount);
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
