36e7527b4be7e44830542b14f7b97dd37cb9da32
Michael Shafer <mshafer3@gmail.com>
2014-10-06 10:54:37 +1300

    Removed unused Start() and Update() methods.

--- LucidDreamer/Assets/scripts/CoinCollectable.cs
+++ LucidDreamer/Assets/scripts/CoinCollectable.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class CoinCollectable : Collectable {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public override void CollectedBehaviour () {
 		Debug.Log ("You got a coin!", this);
 	}
--- LucidDreamer/Assets/scripts/PowerUpCollectable.cs
+++ LucidDreamer/Assets/scripts/PowerUpCollectable.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class PowerUpCollectable : Collectable {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public override void CollectedBehaviour () {
 		Debug.Log ("You got a power-up!", this);
 	}
