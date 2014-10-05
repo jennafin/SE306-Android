7fb3c3021b839a077b19486634ae5c968dfad959
Nicholas Weatherburn <n.weatherburn@gmail.com>
2014-10-05 22:03:05 +1300

    Started basic implementation of a Game Controller.

---
+++ LucidDreamer/Assets/scripts/LevelFactory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelFactory {
+
+	Theme theme;
+	GameObject prefab;
+
+	public LevelFactory() {
+
+	}
+
+	public void setTheme(Theme theme) {
+		this.theme = theme;
+	}
+
+	public void setLevelSegment(GameObject prefab) {
+		this.prefab = prefab;
+	}
+
+	public GameObject build() {
+		return null;
+	}
+}
