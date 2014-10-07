7bfd23f1909f61c803cf95cfedccf39e21a4dc9c
Shay Kalyan <akshaykalyan@outlook.com>
2014-10-07 23:26:51 +1300

    Resized layout for GameOver scene. Set highscore option in MainMenu to temporarily redirect to GameOver scene #15

--- LucidDreamer/Assets/scripts/GameOverScript.cs
+++ LucidDreamer/Assets/scripts/GameOverScript.cs
@@ -2,33 +2,58 @@
 using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
-	
-	int score = 0;
-	private bool retryButton;
-	private bool exitToMenuButton;
+
+	public GUIStyle gameOverStyle;
+
+	private int score = 0;
+
+	private int screenHeight;
+	private int screenWidth;
+	private int buttonWidth;
+	private int buttonHeight;
 
 	// Use this for initialization
 	void Start () {
-		//score = PlayerPrefs.GetInt ("Score");
+		//TODO score = PlayerPrefs.GetInt ("Score");
+		score = 12345;
+
+		screenHeight = Screen.height;
+		screenWidth = Screen.width;
+
+		gameOverStyle.fontSize = (int)(0.06 * screenHeight);
+		gameOverStyle.alignment = TextAnchor.MiddleCenter;
+		buttonWidth = screenWidth / 5;
+		buttonHeight = screenHeight / 10;
+
 	}
 	
 	void OnGUI() {
-		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "GAME OVER");
-		
-		GUI.Label (new Rect(Screen.width /2 - 40, 300, 80, 30), "Score: " + score);
-		
-		retryButton = GUI.Button(new Rect(Screen.width / 2 - 30, 350, 60, 30), "Retry?");
 
-		exitToMenuButton = GUI.Button(new Rect(Screen.width / 2 - 50, 400, 100, 30), "Exit to Menu");
+		GUI.Label (new Rect ((screenWidth / 2 - 50), 50, 80, 30)
+		           , "Alex woke up!"
+		           , gameOverStyle);
+		GUI.Label (new Rect((screenWidth / 2 - 50), screenHeight / 4, 80, 30)
+		           , "Score: " + score
+		           , gameOverStyle);
+
+
+		GUIStyle customButton = new GUIStyle("button");
+		customButton.fontSize = screenHeight / 13;
+
+		if (GUI.Button (new Rect ((screenWidth / 2 - (buttonWidth / 2)), 2 * screenHeight / 4, buttonWidth , buttonHeight)
+		                , "Retry?"
+		                , customButton)) {
+			Application.LoadLevel ("main");
+		}
+
+		if (GUI.Button (new Rect (screenWidth / 2 - buttonWidth, 3 * screenHeight / 4, buttonWidth*2, buttonHeight)
+		                , "Exit to Menu"
+		                , customButton)) {
+			Application.LoadLevel ("MainMenu");
+		}
 	}
 	
 	void Update() {
-		if (retryButton) {
-			Application.LoadLevel ("main");
-		} else if (exitToMenuButton) {
-			Application.LoadLevel ("MainMenu");
-		}
-
 		// go to main menu on escape/back button
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			Debug.Log("GameOverScript: Escape key pressed");
