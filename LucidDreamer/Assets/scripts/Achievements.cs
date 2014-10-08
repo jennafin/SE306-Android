12f3bd3cc2387534cecf01673d5887d5aa98847d
joshofreeness <joshofreeness@gmail.com>
2014-10-09 01:36:27 +1300

    Added achievements script <@joshofreeness | @streetdragon>

---
+++ LucidDreamer/Assets/scripts/Achievements.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public static class Achievements {
+
+	private bool one = false;
+	private bool two = false;
+	private bool three = false;
+	private bool four = false;
+	private bool five = false;
+	private bool six = false;
+
+	public static void GetUnityWorks() {
+		Social.ReportProgress("CgkIj8PyxKwKEAIQAg", 100.0f, (bool success) => {
+			// handle success or failure
+		});
+	}
+	
+	public static void GetRan10Meters() {
+		Social.ReportProgress("CgkIj8PyxKwKEAIQAw", 100.0f, (bool success) => {
+			// handle success or failure
+		});
+	}
+	
+	public static void GetRan20Meters() {
+		Social.ReportProgress("CgkIj8PyxKwKEAIQBA", 100.0f, (bool success) => {
+			// handle success or failure
+		});
+	}
+	
+	public static void GetRan30Meters() {
+		Social.ReportProgress("CgkIj8PyxKwKEAIQBQ", 100.0f, (bool success) => {
+			// handle success or failure
+		});
+	}
+	
+	public static void GetRan40Meters() {
+		Social.ReportProgress("CgkIj8PyxKwKEAIQBg", 100.0f, (bool success) => {
+			// handle success or failure
+		});
+	}
+	
+	public static void GetRan50Meters() {
+		Social.ReportProgress("CgkIj8PyxKwKEAIQBw", 100.0f, (bool success) => {
+			// handle success or failure
+		});
+	}
+}
