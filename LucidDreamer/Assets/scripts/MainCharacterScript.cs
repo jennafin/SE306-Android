1d3751f93aa7116e087f92df21dd1bb09cfb49e9
Nicholas Weatherburn <n.weatherburn@gmail.com>
2014-10-09 13:37:26 +1300

    Updated jump to be consistent and implemented doubleJump.

--- LucidDreamer/Assets/scripts/MainCharacterScript.cs
+++ LucidDreamer/Assets/scripts/MainCharacterScript.cs
@@ -12,23 +12,52 @@ public class MainCharacterScript : MonoBehaviour {
 
 	public GameObject GameController;
 
+	bool hasJumped = false;
+	bool hasDoubleJumped = false;
+	bool isGrounded = false;   // Whether the character is on the ground or not.
 
-	bool grounded = false;   // Whether the character is on the ground or not.
 	public Transform groundCheck;
 	float groundRadius = 0.2f;
 	public LayerMask ground;
 
-	void FixedUpdate() {
-		grounded = IsGrounded ();
-		if (grounded && (Input.GetButton ("Jump") || Input.GetButton("Fire1"))) {
-			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+	void Update() {
+		updateIsGrounded ();
+		bool userPressJump = Input.GetButtonDown ("Jump") || Input.GetButtonDown ("Fire1");
+		if (userPressJump) {
+			if (isGrounded) {
+				Jump ();
+			} else if (!isGrounded && !hasDoubleJumped) {
+				DoubleJump ();
+			}
 		}
 		rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
 	}
 
-	bool IsGrounded ()
-	{
-		return Physics2D.OverlapCircle (groundCheck.position, groundRadius, ground);
+	// Jumps
+	void Jump() {
+		addJumpForce (jumpForce);
+	}
+
+	// Performs a double jump
+	void DoubleJump() {
+		hasDoubleJumped = true;
+		addJumpForce (jumpForce);
+	}
+
+	// Adds a upwards jump force
+	void addJumpForce(float force) {
+		Vector3 velocity = rigidbody2D.velocity;
+		velocity.y = 0;
+		rigidbody2D.velocity = velocity;
+		rigidbody2D.AddForce(new Vector2(0, force));
+	}
+
+	// Checks whether alex is on the ground
+	void updateIsGrounded () {
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, ground);
+		if (isGrounded) {
+			hasDoubleJumped = false;
+		}
 	}
 
 	// Ugly hack used to get projectiles to reduce life points
