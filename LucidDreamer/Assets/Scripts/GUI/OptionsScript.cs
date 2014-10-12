b73d0197f8f907523a212165f655b1c89ff9771a
Jenna Finlayson <jennafinlayson@gmail.com>
2014-10-12 15:28:32 +1300

    styled buttons to fit text

--- LucidDreamer/Assets/Scripts/GUI/OptionsScript.cs
+++ LucidDreamer/Assets/Scripts/GUI/OptionsScript.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class OptionsScript : MonoBehaviour {
 
@@ -8,12 +10,32 @@ public class OptionsScript : MonoBehaviour {
 	private int buttonWidth;
 	private int buttonHeight;
 	private GUIStyle optionsStyle;
+	public Language language = Language.English;
 
 	// Use this for initialization
 	void Start () 
 	{
+		Debug.Log ("OptionsScript: Start");
+		// Initialise language file
+		// If not blank then load it
+		if(File.Exists(Application.persistentDataPath + "/language.dat"))
+		{
+			//Binary formatter for loading back
+			BinaryFormatter bf = new BinaryFormatter();
+			//Get the file
+			FileStream f = File.Open(Application.persistentDataPath + "/language.dat", FileMode.Open);
+			//Load the language
+			language = (Language)bf.Deserialize(f);
+			f.Close();
+		}
+		LanguageManager.LoadLanguageFile(language);
 		screenHeight = Screen.height;
 		screenWidth = Screen.width;
+		optionsStyle = new GUIStyle ();
+		optionsStyle.fontSize = (int)(0.06 * screenHeight);
+		optionsStyle.alignment = TextAnchor.MiddleCenter;
+		buttonWidth = screenWidth / 5;
+		buttonHeight = screenHeight / 10;
 	}
 
 	void OnGUI() 
@@ -21,7 +43,7 @@ public class OptionsScript : MonoBehaviour {
 		GUIStyle customButton = new GUIStyle ("button");
 		customButton.fontSize = screenHeight / 13;
 		
-		if (GUI.Button (new Rect ((screenWidth / 2 - (buttonWidth / 2)), 2 * screenHeight / 4, buttonWidth, buttonHeight)
+		if (GUI.Button (new Rect ((screenWidth / 2 - buttonWidth), 2 * screenHeight / 4, buttonWidth * 2, buttonHeight)
 		                , LanguageManager.GetText ("SelectLanguage")
 		                , customButton)) {
 			Application.LoadLevel ("SelectLanguage");
