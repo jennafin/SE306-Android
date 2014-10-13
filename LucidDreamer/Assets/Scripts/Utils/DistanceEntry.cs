9e9406f73ecbb536e895baedd1700132187432cd
hugobateman <hugobateman@gmail.com>
2014-10-14 09:40:23 +1300

    Began working on achievement that tracks total distance over a players

---
+++ LucidDreamer/Assets/Scripts/Utils/DistanceEntry.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class DistanceEntry {
+	
+	public int distance;
+	
+	public DistanceEntry( int d)
+	{
+		this.distance = d;
+	}
+}
---
+++ LucidDreamer/Assets/Scripts/Utils/TotalDistanceManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Linq;
+
+public class TotalDistanceManager {
+	
+	private DistanceEntry totalDistance;
+	
+	
+	public void Load()
+	{
+		//If not blank then load it
+		if(File.Exists(Application.persistentDataPath + "/distance.dat"))
+		{
+			//Binary formatter for loading back
+			BinaryFormatter bf = new BinaryFormatter();
+			//Get the file
+			FileStream f = File.Open(Application.persistentDataPath + "/distance.dat", FileMode.Open);
+			//Load back the scores
+			totalDistance = (DistanceEntry)bf.Deserialize(f);
+			f.Close();
+		}
+	}
+	
+	public void UpdateDistance(int distance)
+	{
+		totalDistance.distance += distance;
+	}
+	
+	public DistanceEntry GetTotalDistance()
+	{
+		return totalDistance;
+	}
+	
+	public void SaveDistance()
+	{
+		//Get a binary formatter
+		BinaryFormatter bf = new BinaryFormatter();
+		//Create a file
+		FileStream f = File.Open(Application.persistentDataPath + "/distance.dat", FileMode.OpenOrCreate);
+		//Save the scores
+		bf.Serialize(f, totalDistance);
+		f.Close();
+	}
+	
+}
