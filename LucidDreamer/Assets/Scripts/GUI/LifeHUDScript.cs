12b6252c8385c44f65f88105a25cd20c130d2986
Nicholas Weatherburn <n.weatherburn@gmail.com>
2014-10-14 15:09:32 +1300

    Changed the life display to be hearts instead of crosses. Changed sizing of life and score displays. Refactored LifeHUDScript so that the positions and sizes of the HUD elements are relative to screen size.

--- LucidDreamer/Assets/Scripts/GUI/LifeHUDScript.cs
+++ LucidDreamer/Assets/Scripts/GUI/LifeHUDScript.cs
@@ -2,29 +2,57 @@
 using System.Collections;
 using System.Linq;
 
-
-
 public class LifeHUDScript : MonoBehaviour {
-	private string lives = "x x x";
 	public GUIStyle  lifeStyle;
 	private int score = 0;
+	public Texture heart;
+	
+	int numberOfLives;
+	
+	float heartWidth;
+	float heartHeight;
+	float firstHeartStartX;
+	float firstHeartStartY;
+	
 
+	void Start() {
+		heartWidth = Screen.width * 0.02f;
+		heartHeight = Screen.height * 0.03f;
+		firstHeartStartX = heartWidth;
+		firstHeartStartY = heartHeight;
+		
+		numberOfLives = 3; // Default
+	}
+	
 	void OnGUI() {
-		lifeStyle.fontSize = (int)(0.06 * Screen.height);
+		drawLives ();
+		drawScore ();	
+	}
+	
+	void drawScore() {
+		lifeStyle.fontSize = (int) Mathf.Ceil(0.05f * Screen.height);
 		lifeStyle.normal.textColor = Color.red;
-		//Life HUD
-		GUI.TextArea(new Rect(30, 20, 50, 50), lives, lifeStyle);
-
-		//ScoreHUD
-		GUI.TextArea(new Rect(Screen.width-200, 20, 50, 20), score.ToString(), lifeStyle);
-
-
+		
+		Vector2 size = lifeStyle.CalcSize(new GUIContent(score.ToString()));
+		Debug.Log ("String size: " + size.x + " " + size.y);
+		
+		GUI.TextArea(new Rect(Screen.width - size.x - firstHeartStartX, firstHeartStartY / 2, size.x, size.y), score.ToString(), lifeStyle);
+	}
+	
+	void drawLives() {
+		if (! heart) {
+			Debug.Log ("Heart texture needs to be assigned.");
+		} else {
+			for (int i = 0; i < numberOfLives; i++) {
+				GUI.DrawTexture(new Rect(firstHeartStartX + 1.5f * i * heartWidth, firstHeartStartY, heartWidth, heartHeight), heart, ScaleMode.ScaleAndCrop, true); 
+			}
+			Debug.Log ("Screen width: " + Screen.width);
+			Debug.Log ("Screen height: " + Screen.height);
+		}
 	}
 
 	public void SetLives(int lives) {
-		if (lives >= 0) {
-			this.lives = string.Concat(Enumerable.Repeat("x ", lives).ToArray()).Trim();
-		}
+		this.numberOfLives = lives;
 	}
 
 	public void SetScore(int score)
