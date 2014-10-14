e1a80391a5dfa2f862dce74a568105e98b9d5513
Shay Kalyan <akshaykalyan@outlook.com>
2014-10-14 23:44:27 +1300

    Updated Rotator script in preparation for spinning coins. Now has the ability to specify axis of rotation.

--- LucidDreamer/Assets/Scripts/Utils/Rotator.cs
+++ LucidDreamer/Assets/Scripts/Utils/Rotator.cs
@@ -3,12 +3,30 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
+	public float speed = 1.0f;								// speed multiplier for rotation
+	public enum rotationAxis { X, Y, Z };					// enum to dictact axis of rotation
+	public rotationAxis axisOfRotation = rotationAxis.Y;	// 
+	private float x, y, z;									//
 
-		public float speed = 1.0f;
-
-		// Update is called once per frame
-		void Update ()
-		{
-				transform.Rotate (new Vector3 (0, 15, 0) * Time.deltaTime * speed);
+	// initialise rotation axis
+	void Start () 
+	{	
+		switch (axisOfRotation) {
+		case rotationAxis.X:
+			x = 15f;
+			break;
+		case rotationAxis.Y:
+			y = 15f;
+			break;
+		case rotationAxis.Z:
+			z = 15f;
+			break;
 		}
+	}
+
+	// rotates GameObject around the axis of rotation at the speed specified
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (x, y, z) * Time.deltaTime * speed);
+	}
 }
