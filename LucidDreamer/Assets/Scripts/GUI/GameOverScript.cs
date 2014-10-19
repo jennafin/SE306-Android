f0d00981e1c46a5d7b6cacbacc44caac2d0696b9
Shay Kalyan <akshaykalyan@outlook.com>
2014-10-19 23:59:43 +1300

    Added background to game over scene to match other screens. Stylised buttons and input box #54

--- LucidDreamer/Assets/Scripts/GUI/GameOverScript.cs
+++ LucidDreamer/Assets/Scripts/GUI/GameOverScript.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
+[ExecuteInEditMode()]
 public class GameOverScript : MonoBehaviour
 {
 		public Language language = Language.English;
 		public GUIStyle gameOverStyle;
+		public GUIStyle titleTextStyle;
+		public GUIStyle buttonStyle;
+		public GUIStyle inputBoxStyle;
 		private int score = 0;
 		private int screenHeight;
 		private int screenWidth;
@@ -14,6 +18,7 @@ public class GameOverScript : MonoBehaviour
 		private int buttonHeight;
 		private string userName;
 		private HighScoreManager highScores = new HighScoreManager();
+		
 	
 		void Start ()
 		{
@@ -26,8 +31,15 @@ public class GameOverScript : MonoBehaviour
 				screenHeight = Screen.height;
 				screenWidth = Screen.width;
 
+				titleTextStyle.fontSize = (int)(0.16 * screenHeight);
+				titleTextStyle.alignment = TextAnchor.MiddleCenter;
 				gameOverStyle.fontSize = (int)(0.06 * screenHeight);
 				gameOverStyle.alignment = TextAnchor.MiddleCenter;
+				buttonStyle.fontSize =  screenHeight / 13;
+				buttonStyle.alignment = TextAnchor.MiddleCenter;
+				inputBoxStyle.fontSize = screenHeight / 13;
+				inputBoxStyle.alignment = TextAnchor.MiddleCenter;
+
 				buttonWidth = screenWidth / 5;
 				buttonHeight = screenHeight / 10;
 
@@ -49,9 +61,10 @@ public class GameOverScript : MonoBehaviour
 	
 		void OnGUI ()
 		{
-				GUI.Label (new Rect ((screenWidth / 2 - 50), 50, 80, 30)
+					GUI.Label (new Rect (0, screenHeight / 10, screenWidth, 0)
 		           , LanguageManager.GetText ("GameOverScreenMessage")
-		           , gameOverStyle);
+		           , titleTextStyle);
+
 				GUI.Label (new Rect ((screenWidth / 2 - 50), screenHeight / 4, 80, 30)
 		           , LanguageManager.GetText ("Score") + score
 		           , gameOverStyle);
@@ -60,26 +73,25 @@ public class GameOverScript : MonoBehaviour
 		          , LanguageManager.GetText ("TopScore") + highScores.GetTopScore().name + "  " + highScores.GetTopScore().score
 		          , gameOverStyle);
 
+		
 
-				GUIStyle customButton = new GUIStyle ("button");
-				customButton.fontSize = screenHeight / 13;
-
-				if (GUI.Button (new Rect ((screenWidth / 2 - (buttonWidth / 2)), 2.5f * screenHeight / 4, buttonWidth, buttonHeight)
+				if (GUI.Button (new Rect ((screenWidth / 2 - (buttonWidth / 2)), 3.5f * screenHeight / 5, buttonWidth, buttonHeight)
 		                , LanguageManager.GetText ("Retry")
-		                , customButton)) {
+		                , buttonStyle)) {
 						SaveScore();
-						Application.LoadLevel ("main");
+						LoadGame();
 				}
 
-				userName = GUI.TextField(new Rect ((screenWidth / 2 - (buttonWidth / 2)), 2 * screenHeight / 4, buttonWidth, buttonHeight)
-				              , userName, 25, customButton);
+
+				userName = GUI.TextField(new Rect ((screenWidth / 2 - (buttonWidth * (userName.Length / 7.5f) / 2)), 2 * screenHeight / 4, buttonWidth * (userName.Length / 7.5f), buttonHeight)
+		                         , userName, 25, inputBoxStyle);
 
 
-				if (GUI.Button (new Rect (screenWidth / 2 - buttonWidth, 3 * screenHeight / 4, buttonWidth * 2, buttonHeight)
+				if (GUI.Button (new Rect (screenWidth / 2 - buttonWidth, screenHeight - buttonHeight - 20, buttonWidth * 2, buttonHeight)
 		                , LanguageManager.GetText ("ExitToMenu")
-		                , customButton)) {
+		                , buttonStyle)) {
 						SaveScore();
-						Application.LoadLevel ("MainMenu");
+						LoadMainMenu();
 				}
 		}
 
@@ -97,7 +109,15 @@ public class GameOverScript : MonoBehaviour
 			if (Input.GetKeyDown (KeyCode.Escape)) {
 					Debug.Log ("GameOverScript: Escape key pressed");
 					SaveScore();
-					Application.LoadLevel ("MainMenu");
+					LoadMainMenu();
 			}
 		}	
+
+		private void LoadMainMenu() {
+			GameObject.Find ("Main Camera").GetComponent<SceneFader> ().LoadScene("MainMenu");
+		}
+
+		private void LoadGame() {
+			GameObject.Find ("Main Camera").GetComponent<SceneFader> ().LoadScene("main");
+		}
 }
