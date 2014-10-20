8298bdf3f7944fc03389d8642930a4e713759b86
joshofreeness <joshofreeness@gmail.com>
2014-10-20 15:36:49 +1300

    Fixed lucid power, now powers down

--- LucidDreamer/Assets/Scripts/Controllers/ShakeDetectorScript.cs
+++ LucidDreamer/Assets/Scripts/Controllers/ShakeDetectorScript.cs
@@ -156,7 +156,7 @@ public class ShakeDetectorScript : MonoBehaviour {
 //					obj.doShakeAction ();
 //				}
 //			}
-//			PowerDown = true;
+			PowerDown = true;
 //			Debug.Log ("hahaha");
 			gameController.CollectAllCollectables();
 
