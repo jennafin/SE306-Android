b6edad5185197b42c80c8ea85a295f4258d4d6ad
Jamie Peterson <jampetnz@gmail.com>
2014-10-21 01:25:27 +1300

    Added some damping to the camera movement, adjusted lighting a bit

---
+++ LucidDreamer/Assets/Scripts/Controllers/Camera3DMoveScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera3DMoveScript : MonoBehaviour
+{
+
+	public Transform player;
+	public float distanceBehind;
+	public float distanceAbove;
+	public float verticalFluidity;
+	
+	void Start ()
+	{
+		UpdateOffsets ();
+	}
+
+	void Update ()
+	{
+		UpdateOffsets();
+
+	}
+
+	void UpdateOffsets ()
+	{
+		float verticalTarget = player.position.y + distanceAbove;
+		float verticalDelta = verticalTarget - transform.position.y;
+		float verticalPosition = verticalTarget - verticalDelta * Mathf.Pow (verticalFluidity, Time.deltaTime);
+
+		// Update the camera transform to follow the player
+		transform.position = new Vector3 (player.position.x - distanceBehind, verticalPosition, player.position.z);
+	}
+}
