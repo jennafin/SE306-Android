5cb2b2a18b71b8d69f65f67e87f7559879aab0a9
Shay Kalyan <akshaykalyan@outlook.com>
2014-10-21 10:38:30 +1300

    Styled paused button #109

--- LucidDreamer/Assets/Scripts/GUI/PauseMenuScript.cs
+++ LucidDreamer/Assets/Scripts/GUI/PauseMenuScript.cs
@@ -8,19 +8,18 @@ public class PauseMenuScript : MonoBehaviour {
 
 	public GUIStyle buttonStyle;
 	public GUIStyle titleTextStyle;
-	public Texture fadeOverlayTexture;
-	public float fadeSpeed = 0.8f;
+	public Texture fadeOverlayTexture;				// texture for the pause menu's background
 	public Language language = Language.English;
-	private bool isPaused = false;
+	
+	private bool isPaused = false;			
+	private GUIStyle pausedButtonStyle;
 	private int screenHeight;
 	private int screenWidth;
 	private int buttonWidth;
 	private int buttonHeight;
-	private int drawDepth = -1000;			// texture's drawing order in hierarchy - lower number renders to top
-	private float alpha = 1.0f;				// texture's alpha
-	private int fadeDir = -1;				// direction to fade: in = -1, out = 1
+	private int drawDepth = -1000;			// overlay's drawing order in hierarchy - lower number renders to top
+	private float alpha = 1.0f;				// alpha of pause overlay
 
-	// Use this for initialization
 	void Start () {
 		// Initialise language file
 		// If not blank then load it
@@ -41,6 +40,8 @@ public class PauseMenuScript : MonoBehaviour {
 
 		buttonStyle.fontSize =  screenHeight / 13;
 		buttonStyle.alignment = TextAnchor.MiddleCenter;
+		pausedButtonStyle = buttonStyle;
+		pausedButtonStyle.fontSize = screenHeight / 25;
 		titleTextStyle.fontSize = (int)(0.16 * screenHeight);
 		titleTextStyle.alignment = TextAnchor.MiddleCenter;
 
@@ -51,12 +52,14 @@ public class PauseMenuScript : MonoBehaviour {
 	void OnGUI() {
 		if (isPaused) {
 
-
 			GUI.Label (new Rect (0, screenHeight / 10, screenWidth, 0)
-	           , LanguageManager.GetText ("Paused")
-	           , titleTextStyle);
+	           			, LanguageManager.GetText ("Paused")
+	           			, titleTextStyle);
 
-			GUILayout.BeginArea (new Rect (screenWidth / 2 - screenWidth / 4, 5.5f * screenHeight / 13, screenWidth / 2, screenHeight));
+			GUILayout.BeginArea (new Rect (screenWidth / 2 - screenWidth / 4
+											, 5.5f * screenHeight / 13
+											, screenWidth / 2
+											, screenHeight));
 			GUILayout.BeginVertical ();
 			if (GUILayout.Button (LanguageManager.GetText ("Resume"), buttonStyle)) {
 				isPaused = false;
@@ -76,37 +79,37 @@ public class PauseMenuScript : MonoBehaviour {
 
 		} else {
 			FadeOut();
-			if (GUI.Button(new Rect(screenWidth - (buttonWidth/4), buttonHeight, buttonWidth/4, buttonHeight), "")) {
+			if (GUI.Button(new Rect(screenWidth - (buttonWidth/3)
+										, buttonHeight
+										, buttonWidth/4
+										, buttonHeight)
+										, " ▌▌"
+										, pausedButtonStyle)) {
 				isPaused = true;
 				FadeIn();
 				GameObject.Find ("GameController").GetComponent<GameControllerScript> ().PauseGame();
 			}
 		}
 
-//		// fade out/in alpha based on direction, speed and time
-//		alpha += fadeDir * fadeSpeed * Time.deltaTime;
-//		// clamp alpha between 0 and .5
-//		alpha = Mathf.Clamp (alpha, 0f, 0.5f); 
-
-
-		// set color of GUI
-		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha); 			// set alpha
-		GUI.depth = drawDepth;															// set black texture on top
+		// set faded overlay for pause menu
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha); 				// set alpha
+		GUI.depth = drawDepth;																// set black texture on top
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeOverlayTexture); // draw texture to fit entire screen
 			
 	}
 
+	// loads the MainMenu scene using the SceneFade script attached to the GameController GameObject
 	private void LoadMainMenu() {
 		GameObject.Find ("GameController").GetComponent<SceneFader> ().LoadScene("MainMenu");
 	}
 
+	// sets the alpha value of the pause overlay texture to be visible
 	private void FadeIn() {
 		alpha = 0.3f;
-//		fadeDir = 1;
 	}
 
+	// sets the alpha value of the pause overlay to zero
 	private void FadeOut() {
 		alpha = 0.0f;
-//		fadeDir = -1;
 	}
 }
