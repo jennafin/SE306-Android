2efd2580f33dff5f44c181983e8bc60229b1c012
Shay Kalyan <akshaykalyan@outlook.com>
2014-10-21 16:11:39 +1300

    Fixed Alex jumping when interacting with pause menu / pause button #109

--- LucidDreamer/Assets/Scripts/Characters/MainCharacterScript.cs
+++ LucidDreamer/Assets/Scripts/Characters/MainCharacterScript.cs
@@ -30,15 +30,17 @@ public class MainCharacterScript : MonoBehaviour {
 	private bool soundEffectsOn;
 	
 	private bool isPaused = false;	// Whether the game is paused currently or not
+	private Rect touchArea;
 	
 	void Start() {
 		this.currentJumpForce = jumpForce;
 		RetrieveSettings ();
+		touchArea = new Rect(0, 0, Screen.width, (Screen.height - Screen.height/5));
 	}
 
 	void Update() {
 		updateIsGrounded ();
-		bool userPressJump = (Input.GetButtonDown ("Jump") || Input.GetButtonDown ("Fire1")) && !isPaused;
+		bool userPressJump = (Input.GetButtonDown ("Jump") || JumpAreaTouched()) && !isPaused;
 		if (userPressJump) {
 			if (isGrounded) {
 				Jump ();
@@ -129,5 +131,15 @@ public class MainCharacterScript : MonoBehaviour {
 	public void UnpauseJumpAbility() {
 		isPaused = false;
 	}
-
+	
+	// checks if user input is within bounds of the area for triggering a jump
+	private bool JumpAreaTouched() {
+		if (Input.touchCount == 1 && (Input.GetTouch(0).phase == TouchPhase.Began)) {
+			if (touchArea.Contains(Input.GetTouch(0).position)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
