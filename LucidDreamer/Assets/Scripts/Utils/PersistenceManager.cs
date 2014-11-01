ef8741d1ab263528d73c0cbef8688205cbe303d9
Nicholas Weatherburn <n.weatherburn@gmail.com>
2014-11-01 14:28:55 +1300

    Possible fix for Alex getting stuck. Removed unnecessary debug statements.

--- LucidDreamer/Assets/Scripts/Utils/PersistenceManager.cs
+++ LucidDreamer/Assets/Scripts/Utils/PersistenceManager.cs
@@ -12,7 +12,6 @@ public class PersistenceManager : MonoBehaviour {
 	
 	public void Load()
 	{
-		print (Application.persistentDataPath);
 		//If not blank then load it
 		if(File.Exists(Application.persistentDataPath + "/totalScore.dat"))
 		{
